3a8fc439d3887d110cf8f5de56fd6b5f3b793112
Valdy NGOUABIRA <valdyngouabira@gmail.com>
2024-04-25 21:59:53 +0000

    wip

--- src/pages/protected/DashBboard.tsx
+++ src/pages/protected/DashBboard.tsx
@@ -1,20 +1,10 @@
-import { useEffect } from 'react'
-import { useJwt } from 'react-jwt';
-import { getToken } from '../../utils/helper';
-import AuthResponse from '../../core/models/auth.response';
 
 function DashBboard() {
-  const token = getToken();
-  const { decodedToken, isExpired } = useJwt<AuthResponse>(token);
 
-  useEffect(()=>{
-    console.log(token);
-    console.log(decodedToken?.role);
-  },[])
   
 
   return (
-    <div>{isExpired}</div>
+    <div>Dashboard</div>
   )
 }
 
